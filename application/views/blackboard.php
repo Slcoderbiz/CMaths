258bb09fc4fc4d4deb6f7e43536353733c7966ca
SLCODER <Slcoderbiz@gmail.com>
2016-10-25 23:19:55 +0530

    new

--- application/views/blackboard.php
+++ application/views/blackboard.php
@@ -1,11 +1,96 @@
-<html>
+<html xmlns="http://www.w3.org/1999/html">
 <head>
 
+    <link href="<?= base_url(); ?>/assets/css/bootstrap.min.css" rel="stylesheet">
 
+    <!-- Custom Google Web Font -->
+    <link href="<?= base_url(); ?>/assets/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link href='<?= base_url(); ?>/assets/http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css'>
+    <link href='<?= base_url(); ?>/assets/http://fonts.googleapis.com/css?family=Arvo:400,700' rel='stylesheet' type='text/css'>
+
+    <!-- Custom CSS-->
+    <link href="<?= base_url(); ?>/assets/css/general.css" rel="stylesheet">
 
 
 </head>
-<body>
+<body style="background-image: url('<?= base_url(); ?>/assets/img/slide/Macbook_iPhone-6_Lady_Technology_UHD.jpg');background-size: cover;>
+<nav class="navbar navbar-inverse">
+<nav class="navbar-default" role="navigation">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="<?=base_url();?>">සංයුක්ත ගණිතය </a>
+        </div>
 
+
+
+    </div>
+</nav>
+
+<br>
+<br>
+<div class="container-fluid">
+<div class="row">
+<div class="col-sm-8" >
+    <div class="panel panel-default" style="opacity: 0.9;color: #8e44ad">
+        <div class="panel-heading"><h2><center>සතියේ පැනය</center></h2></div>
+        <div class="panel-body"><b>Volume V (V = 0.8314 dm3
+                ) of each of the liquids A and B were added to an evacuated vessel of volume
+                100.8 V and allowed to reach the equilibrium. At 300 K, the total pressure of the system was found
+                to be 3.00 × 105
+                Pa. At 300 K, the molar volumes of liquids A and B are 8.314 × 10ï2
+                dm3
+                molï1
+                and
+                4.157 × 10ï2
+                dm3
+                molï1
+                respectively. The solution of A and B behave ideally.<br>
+                (i) Calculate separately the amounts of moles of A and B mixed.<br>
+                (ii) Calculate separately the partial vapour pressures of A and B if the mole fraction of A in the vapour
+                phase was 0.2.</br></b></div>
+    </div>
+
+</div>
+<div class="col-sm-4"></div>
+</div>
+</div>
+<br>
+
+<div class="container-fluid">
+<div class="row">
+<div class="col-sm-8 col-sm-offset-4" >
+    <div class="panel panel-default" style="opacity: 0.9;">
+        <div class="panel-body" style="color: #0000FF"><b> (i) number of moles of A" QA = 0.8314 dm3
+                8.314 × 10ï2 dm3molï1 = 10mol
+                number of moles of B" QB = 0.8314 dm3
+                4.157 × 10ï2 dm3molï1 = 20mol<br>
+
+                (ii) A(g)
+                A(l)
+                B(g)
+                B(l)
+                XA(g) = 0.2
+                assuming the ideal behaviour of the vapour,
+                PA = XA(g) . PT
+                = 0.2 (3 × 105 Pa)
+                PA = 6 × 104 Pa </br></div>
+        <div class="panel-footer"><h2><center>පිලිතුර</center></h2></div>
+    </div>
+</div>
+
+</div>
+</div>
+
+
+<script src="<?= base_url(); ?>/assets/js/jquery-1.10.2.js"></script>
+<script src="<?= base_url(); ?>/assets/js/bootstrap.js"></script>
+<script src="<?= base_url(); ?>/assets/js/owl.carousel.js"></script>
+<script src="<?= base_url(); ?>/assets/js/script.js"></script>
 </body>
 </html>
